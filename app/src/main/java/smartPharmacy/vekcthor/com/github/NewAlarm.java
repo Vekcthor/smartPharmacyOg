a72d52acacc2ab9544730bb9331d5e4bea335e85
Victor Azevedo <victor.sil.azevedo@gmail.com>
2021-10-17 15:12:29 -0300

    Primeiro commit

---
+++ app/src/main/java/smartPharmacy/vekcthor/com/github/NewAlarm.java
@@ -0,0 +1,14 @@
+package smartPharmacy.vekcthor.com.github;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_alarm);
+    }
+}
