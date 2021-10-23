7423d3fd8a9cc9bc0c73cc930a9717e869792715
Victor Azevedo <victor.sil.azevedo@gmail.com>
2021-10-23 13:24:34 -0300

    Melhorias na tela de NewAlarm, se aproximando mais do layout e funcionalidades finais

--- app/src/main/java/smartPharmacy/vekcthor/com/github/NewAlarm.java
+++ app/src/main/java/smartPharmacy/vekcthor/com/github/NewAlarm.java
@@ -1,14 +1,65 @@
 package smartPharmacy.vekcthor.com.github;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+import java.util.List;
+
+import ca.antonious.materialdaypicker.MaterialDayPicker;
+
 public class NewAlarm extends AppCompatActivity {
+    TimePickerDialog picker;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_alarm);
+
+        editText=(EditText)findViewById(R.id.editTextHourMedication);
+        final MaterialDayPicker materialDayPicker = findViewById(R.id.day_picker);
+        materialDayPicker.setDaySelectionChangedListener(new MaterialDayPicker.DaySelectionChangedListener() {
+            @Override
+            public void onDaySelectionChanged(@NonNull List<MaterialDayPicker.Weekday> selectedDays) {
+               System.out.println(String.format("[DaySelectionChangedListener]%s", selectedDays.toString()));
+            }
+        });
+
+        editText.setInputType(InputType.TYPE_NULL);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minutes = calendar.get(Calendar.MINUTE);
+                // time picker dialog
+                picker = new TimePickerDialog(NewAlarm.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                                editText.setText(sHour + ":" + sMinute);
+                            }
+                        }, hour, minutes, true);
+                picker.show();
+            }
+        });
+        button=(Button)findViewById(R.id.buttonSaveNewAlarm);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //textView.setText("Selected Time: "+ editText.getText());
+            }
+        });
     }
 }
